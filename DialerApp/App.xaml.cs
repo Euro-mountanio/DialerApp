48a8c1a6c45daf516ca027059ab4d9534cf2e0f0
euro <oscarphiri25@gmail.com>
2024-04-08 21:22:35 +0200

    Add project files.

---
+++ DialerApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DialerApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DialerApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DialerApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
